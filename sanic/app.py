045c0a4c70d09b2161fa78bc4129f36cf9d5c388
banjocat <jackmuratore@gmail.com>
2017-01-02 00:15:23 -0500

    I must enjoy torturing myself

--- sanic/app.py
+++ sanic/app.py
@@ -3,10 +3,14 @@ import os
 from sanic import Sanic
 from sanic.response import json
 from kafka import KafkaProducer
+from confluent_kafka import Producer
 
 app = Sanic()
 kafka_host = os.getenv('KAFKA_HOST').split(',')
 producer = KafkaProducer(bootstrap_servers=kafka_host)
+
+confluent_producer = Producer({'bootstrap.servers': 'kafka1'})
+
 
 @app.route('/')
 async def index(request):
@@ -14,8 +18,14 @@ async def index(request):
 
 @app.route('/kafka')
 async def kafka_producer(request):
-    await producer.send('json', request.body)
+    producer.send('json', request.body)
     return json({"message": "Success"})
+
+@app.route('/confluent')
+async def confluent_producer(request):
+    confluent_producer.produce('json', request.body)
+    return json({"message": "Success"})
+
 
 
 if __name__ == '__main__':
